287c7cdbf5381d2029f72f118ca4f327d4a69f55
ErikAnkerKilbergSkallevold <erik.skallevold@gmail.com>
2022-08-26 22:32:40 +0200

    All exercises completed 🎉🎉🎉🦀🦀
    Yeah

---
+++ exercises/hashmaps/hashmaps1.rs
@@ -0,0 +1,62 @@
+// hashmaps1.rs
+// A basket of fruits in the form of a hash map needs to be defined.
+// The key represents the name of the fruit and the value represents
+// how many of that particular fruit is in the basket. You have to put
+// at least three different types of fruits (e.g apple, banana, mango)
+// in the basket and the total count of all the fruits should be at
+// least five.
+//
+// Make me compile and pass the tests!
+//
+// Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
+
+
+use std::collections::HashMap;
+
+fn fruit_basket() -> HashMap<String, u32> {
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
+
+    // Two bananas are already given for you :)
+    basket.insert(String::from("banana"), 2);
+
+    // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("apple"), 1);
+    basket.insert(String::from("mango"), 2);
+    basket.insert(String::from("orange"), 5);
+    basket.insert(String::from("grape"), 2);
+    basket.insert(String::from("strawberry"), 3);
+    basket.insert(String::from("blueberry"), 4);
+    basket.insert(String::from("raspberry"), 1);
+    basket.insert(String::from("blackberry"), 2);
+    basket.insert(String::from("coconut"), 3);
+    basket.insert(String::from("pineapple"), 4);
+    basket.insert(String::from("pomegranate"), 5);
+    basket.insert(String::from("watermelon"), 6);
+    basket.insert(String::from("kiwi"), 7);
+    basket.insert(String::from("cantaloupe"), 8);
+    basket.insert(String::from("pear"), 9);
+    basket.insert(String::from("cherry"), 10);
+    basket.insert(String::from("peach"), 11);
+    basket.insert(String::from("apricot"), 12);
+    basket.insert(String::from("avocado"), 13);
+
+
+    basket
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn at_least_three_types_of_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.len() >= 3);
+    }
+
+    #[test]
+    fn at_least_five_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.values().sum::<u32>() >= 5);
+    }
+}
